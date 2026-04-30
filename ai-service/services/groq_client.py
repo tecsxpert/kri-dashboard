d75a82db36e9823d3eab9a38fb33b8346c55af48
Gunavathi HR <gunavathihr124@gmail.com>
2026-04-30 16:27:03 +0530

    Updated ai-service with my code

--- ai-service/services/groq_client.py
+++ ai-service/services/groq_client.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-<<<<<<< HEAD
 class GroqClient:
     def __init__(self):
         self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
@@ -25,21 +24,3 @@ class GroqClient:
         except Exception as e:
             print("Error:", e)
             return "AI service unavailable"
-=======
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
-def generate_response(prompt):
-    try:
-        response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
-            messages=[
-                {"role": "user", "content": prompt}
-            ],
-            temperature=0.5
-        )
-        return response.choices[0].message.content
-
-    except Exception as e:
-        return f"ERROR: {str(e)}"
-    print("API KEY:", os.getenv("GROQ_API_KEY"))
->>>>>>> c33a9c2732f54cd81b9201aa294c09b33b2cedf7
