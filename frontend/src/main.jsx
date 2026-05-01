9f5fc7be128426e4733fbfcdb372920a84503c5d
rashi-2111 <rashise22125@gmail.com>
2026-05-01 19:39:04 +0530

    Day 13 - frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import AuthProvider from "./context/AuthContext";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+import App from "./App.jsx";
+import AuthProvider from "./context/AuthContext";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
 );
